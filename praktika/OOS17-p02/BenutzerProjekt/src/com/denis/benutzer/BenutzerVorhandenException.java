d2561e08cacc240f9742ad9e9f8c4c7c41657276
Code1x1 <the.programming.tutorial.101@gmail.com>
2017-11-12 21:22:57 +0100

    BenutzerVorhandenException Klasse Kommentar hinzugefügt

--- praktika/OOS17-p02/BenutzerProjekt/src/com/denis/benutzer/BenutzerVorhandenException.java
+++ praktika/OOS17-p02/BenutzerProjekt/src/com/denis/benutzer/BenutzerVorhandenException.java
@@ -1,5 +1,16 @@
 package com.denis.benutzer;
 
+/**
+ * 
+ * <p>Überschrift: BenutzerVorhandenException</p>
+ * <p>Beschreibung: Diese Klasse definiert die Exception BenutzerVorhandenException.java.
+ * 					Diese Exception soll geworfen werden, wenn ein Benutzer existiert/ nicht existiert.</p>
+ *  
+ * <p>Copyright: Denis (c) 2017</p>
+ * <p>Organisation: FH Aachen, FB05</p>
+ * @author Denis
+ *
+ */
 public class BenutzerVorhandenException extends Exception {
 
 	public BenutzerVorhandenException(String s) {
